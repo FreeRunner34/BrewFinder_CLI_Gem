6131822ef9e109b9d86f5701b641e946206778a2
FreeRunner34 <64115389+FreeRunner34@users.noreply.github.com>
2020-12-08 10:05:02 -0800

    Add files via upload

---
+++ brewfinder.rb
@@ -0,0 +1,49 @@
+class Brewery
+
+  attr_accessor :id, :name, :brewery_type, :street, :address_2, :address_3, :city, :state, :county_province, :postal_code, :country, :longitude, :latitude, :phone, :website_url, :updated_at, :created_at
+
+  @@all = []
+
+  def initialize(attributes)
+      attributes.each do |key, value| 
+          self.class.attr_accessor(key)
+          self.send(("#{key}="), value)
+          end
+          @@all << self 
+  end 
+
+  def self.all 
+      @@all 
+  end
+
+  def save 
+      @@all << self 
+  end 
+
+  def self.reset_all
+      @@all.clear
+  end 
+
+  def self.count 
+      @@all.size
+  end 
+  #binding.pry
+end 
+
+# id
+# name
+# brewery_type
+# street
+# address_2
+# address_3
+# city
+# state
+# county_province
+# postal_code
+# country
+# longitude
+# latitude
+# phone
+# website_url
+# updated_at
+# created_at
