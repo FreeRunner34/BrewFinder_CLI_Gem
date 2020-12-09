60a7ab93be26b7296f3a11e77f01553992dcd549
FreeRunner34 <64115389+FreeRunner34@users.noreply.github.com>
2020-12-09 12:13:28 -0800

    Update cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,3 @@
-require_relative 'api.rb'
-require_relative 'brewfinder.rb'
 # require 'pry'
 
 class Cli
@@ -7,27 +5,17 @@ class Cli
     def menu
         
         Brewfinder.new.breweries
+        running_loop
         
-        puts "option 1: see a list of Breweries"
-        #other options to be updated and created later. 
-        # puts "option 2: search a Brewery by Name"
-        # puts "option 3: search a Brewery by City"
-        # puts "option 4: search a Brewery by State"
-        # puts "option 5: search a Brewery by Postal Code"
-
-        input = gets.chomp  
-        if input == "1"
-            option_1
-            puts "Please select the number of a brewery you'd like to see more about"
-            prompt = gets.chomp.to_i
-            selected_brewery = Brewery.all[prompt-1]
-                puts selected_brewery.website_url
-        else 
-            exit 
-        end 
+        # puts "option 1: see a list of Breweries"
+        # #other options to be updated and created later. 
+        # # puts "option 2: search a Brewery by Name"
+        # # puts "option 3: search a Brewery by City"
+        # # puts "option 4: search a Brewery by State"
+        # # puts "option 5: search a Brewery by Postal Code"
     end 
 
-    def option_1
+    def list_breweries
         counter = 1 
         Brewery.all.each do |brewery| 
            puts "#{counter}" + ". " + brewery.name
@@ -35,4 +23,29 @@ class Cli
         end 
     end 
 
+    def running_loop
+        loop do 
+            list_breweries
+            puts"Choose a number you'd like to see more about or type exit to quit."
+            input = gets.chomp
+            if input == "exit"
+                break
+            elsif input.to_i.between?(1,Brewery.all.size)
+                brewery_details(input)
+                sleep(5)
+            else 
+                puts "invalid input try again"
+            end 
+            sleep(1)
+        end 
+    end 
+
+    def brewery_details(input)
+        selected_brewery = Brewery.all[input.to_i-1]
+        puts selected_brewery.phone 
+        puts selected_brewery.website_url
+        puts selected_brewery.street
+        puts selected_brewery.city
+        puts selected_brewery.state
+    end 
 end 
